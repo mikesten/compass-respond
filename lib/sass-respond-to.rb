d70ff134c286c6bd25a87949e6d94e7a0a28d051
Mike Stenhouse <mike@donotremove.co.uk>
2012-05-16 18:51:15 +0100

    Consistency.

--- lib/sass-respond-to.rb
+++ lib/sass-respond-to.rb
@@ -1,2 +1,2 @@
-Compass::Frameworks.register('respond-to',
+Compass::Frameworks.register('compass-respond-to',
                              :stylesheets_directory => File.join(File.dirname(__FILE__), 'stylesheets'))
